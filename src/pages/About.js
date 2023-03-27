74a7814906a759724a821b5ab722d117211428f8
erwannbruneteau <erwannthomas@gmail.com>
2023-03-27 21:56:49 +0200

    add logo et style nav

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import Navigation from '../components/Navigation';
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
 
 const About = () => {
-    return (
-        <div>
-            <Navigation />
-            <h1>A propos</h1>
-            <br />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat veritatis sit aspernatur eum eaque earum eius error assumenda ullam eos maiores, incidunt nam dolor mollitia unde accusamus consequuntur sapiente quos nemo at cum perspiciatis deserunt ex. Voluptatem asperiores officiis illo temporibus quis quos dolore minima eaque sit soluta recusandae, sunt saepe voluptas, nihil culpa! Blanditiis amet repellendus fugit repudiandae fugiat iure porro, error dicta ducimus vel labore impedit iusto quod, odio dolorem sit laboriosam sunt, numquam natus? Est optio architecto magni possimus itaque hic neque ab, totam consequatur perferendis blanditiis quaerat minima corrupti laboriosam inventore dolores voluptatem esse accusamus impedit!</p>
-            <br />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eos aliquid repellendus dolore! Consectetur nostrum voluptates, atque culpa dolorem dolorum minima quam mollitia modi ea, quos veritatis architecto nam? Illo nulla iste nobis optio. Nulla soluta corrupti perspiciatis consequatur dolore, similique alias nihil eos dicta voluptatibus minus vitae aperiam quas praesentium incidunt, optio quidem modi officiis quod sit unde sapiente nesciunt. Soluta, exercitationem vitae, sapiente repellendus et possimus, labore dolorum optio ab error odio officia eius voluptatum natus voluptate maxime dolores harum consectetur ea! Atque unde placeat vero ipsa reiciendis? Quod excepturi commodi maiores omnis beatae fugiat est, necessitatibus, molestias libero, doloremque minus cum cumque. Totam maiores corporis aliquid unde similique amet voluptas modi. Praesentium odit facere saepe doloribus eaque neque velit voluptate vero labore consequatur vel exercitationem excepturi distinctio iure dignissimos consequuntur repudiandae adipisci voluptatem, natus explicabo commodi libero unde! Explicabo cupiditate aperiam accusamus molestiae esse harum omnis eum vero! Vitae quisquam id labore sit sint explicabo commodi magni quos delectus itaque, architecto nulla ipsa. Dolorum asperiores est aperiam, minima amet voluptates veniam eaque mollitia animi fugit! Quod cum repudiandae fugit eius impedit. A illum aliquam repellendus placeat, ut consequatur inventore, quod alias, dolores minima voluptatibus magnam eum esse!</p>
-        </div>
-    );
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        veritatis sit aspernatur eum eaque earum eius error assumenda ullam eos
+        maiores, incidunt nam dolor mollitia unde accusamus consequuntur
+        sapiente quos nemo at cum perspiciatis deserunt ex. Voluptatem
+        asperiores officiis illo temporibus quis quos dolore minima eaque sit
+        soluta recusandae, sunt saepe voluptas, nihil culpa! Blanditiis amet
+        repellendus fugit repudiandae fugiat iure porro, error dicta ducimus vel
+        labore impedit iusto quod, odio dolorem sit laboriosam sunt, numquam
+        natus? Est optio architecto magni possimus itaque hic neque ab, totam
+        consequatur perferendis blanditiis quaerat minima corrupti laboriosam
+        inventore dolores voluptatem esse accusamus impedit!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eos
+        aliquid repellendus dolore! Consectetur nostrum voluptates, atque culpa
+        dolorem dolorum minima quam mollitia modi ea, quos veritatis architecto
+        nam? Illo nulla iste nobis optio. Nulla soluta corrupti perspiciatis
+        consequatur dolore, similique alias nihil eos dicta voluptatibus minus
+        vitae aperiam quas praesentium incidunt, optio quidem modi officiis quod
+        sit unde sapiente nesciunt. Soluta, exercitationem vitae, sapiente
+        repellendus et possimus, labore dolorum optio ab error odio officia eius
+        voluptatum natus voluptate maxime dolores harum consectetur ea! Atque
+        unde placeat vero ipsa reiciendis? Quod excepturi commodi maiores omnis
+        beatae fugiat est, necessitatibus, molestias libero, doloremque minus
+        cum cumque. Totam maiores corporis aliquid unde similique amet voluptas
+        modi. Praesentium odit facere saepe doloribus eaque neque velit
+        voluptate vero labore consequatur vel exercitationem excepturi
+        distinctio iure dignissimos consequuntur repudiandae adipisci
+        voluptatem, natus explicabo commodi libero unde! Explicabo cupiditate
+        aperiam accusamus molestiae esse harum omnis eum vero! Vitae quisquam id
+        labore sit sint explicabo commodi magni quos delectus itaque, architecto
+        nulla ipsa. Dolorum asperiores est aperiam, minima amet voluptates
+        veniam eaque mollitia animi fugit! Quod cum repudiandae fugit eius
+        impedit. A illum aliquam repellendus placeat, ut consequatur inventore,
+        quod alias, dolores minima voluptatibus magnam eum esse!
+      </p>
+    </div>
+  );
 };
 
 export default About;
