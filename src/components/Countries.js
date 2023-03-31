87ccd6842e68d9b24c6807fe762b1ded518dbb2c
erwannbruneteau <erwannthomas@gmail.com>
2023-03-31 14:57:52 +0200

    tri par population avec data.sort

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -30,15 +30,19 @@ const Countries = () => {
               type="radio"
               id={continent}
               name="continentRadio"
+              checked={continent === selectedRadio}
               onChange={(e) => setSelectedRadio(e.target.id)}
             />
             <label htmlFor={continent}>{continent}</label>
           </li>
         ))}
       </ul>
+      {selectedRadio && (
+      <button onClick ={() => setSelectedRadio('')} >Annuler la recherche</button>)}
       <ul>
         {data
           .filter((country) => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
           .slice(0, rangeValue)
           .map((country, index) => (
             <Card key={index} country={country} />
